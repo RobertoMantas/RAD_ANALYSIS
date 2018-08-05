b5b8fa1f87d65d203b6230416c9da163f023116d
Roberto <r.mantas.nakhai@gmail.com>
2018-08-05 22:20:38 +0200

    Means_max_min WORKING

--- RAD2.py
+++ RAD2.py
@@ -24,12 +24,15 @@ def is_number(s):
     return False
 
 # Path from where I am reading
-# in_dir = "/Users/robertomantas/Documents/LTU/RAD/RAD_ANALYSIS/RAW_ALL/"
-# out_dir = "/Users/robertomantas/Documents/LTU/RAD/RAD_ANALYSIS/CLEAN_ALL/"
 
-in_dir = "/Users/robertomantas/Documents/GitHub/RAD_ANALYSIS/RAW_ALL/"
-out_dir = "/Users/robertomantas/Documents/GitHub/RAD_ANALYSIS/CLEAN_ALL/"
+in_dir = "/Users/robertomantas/Documents/GitHub/RAD_ANALYSIS/PDS_out/1_AWK/RAW_ALL/"
+out_dir = "/Users/robertomantas/Documents/GitHub/RAD_ANALYSIS/PDS_out/1_AWK/CLEAN_ALL/"
 
+in_dir = "/Users/robertomantas/Documents/GitHub/RAD_ANALYSIS/PDS_out/1_AWK/RAW_ALL/"
+out_dir = "/Users/robertomantas/Documents/GitHub/RAD_ANALYSIS/PDS_out/1_AWK/CLEAN_ALL/"
+
+in_dir = "/data/RAD/PDS_out/1_AWK/RAW_ALL/"
+out_dir = "/data/RAD/PDS_out/1_AWK/CLEAN_ALL/"
 
 def read_clean_MD ():
 
@@ -53,7 +56,6 @@ def read_clean_MD ():
                     output_file.write("[1]START_OBS [2]DOSIMETRY_B_OBS [3]DOSIMETRY_B_DATA [4]DOSIMETRY_E_OBS [5]DOSIMETRY_E_DATA \n")
                     f = 0
                     q = 0
-                    hora_post = 0
 
                     for k in input_file1:
                         
@@ -63,10 +65,6 @@ def read_clean_MD ():
                                #print(col)
                                 hora = col[1][0:8]
                                 
-                                # if hora != hora_post:
-                                    
-                                #     output_file.write(col[1] + ' ')
-                                # hora_post = hora
                             if col[0] == "[DOSIMETRY_TOTAL_DOSE_B:" or f == 1:
                                 #print(col)
                                 
@@ -120,3 +118,89 @@ def read_clean_MD ():
 
                         
 read_clean_MD()
+
+
+in_dir = "/Users/robertomantas/Documents/GitHub/RAD_ANALYSIS/PDS_out/1_AWK/CLEAN_ALL/"
+out_dir = "/Users/robertomantas/Documents/GitHub/RAD_ANALYSIS/PDS_out/2_MEAN_MAX_MIN/"
+
+in_dir = "/Users/robertomantas/Documents/GitHub/RAD_ANALYSIS/PDS_out/1_AWK/CLEAN_ALL/"
+out_dir = "/Users/robertomantas/Documents/GitHub/RAD_ANALYSIS/PDS_out/2_MEAN_MAX_MIN/"
+
+in_dir = "/data/RAD/PDS_out/1_AWK/CLEAN_ALL/"
+out_dir = "/data/RAD/PDS_out/2_MEAN_MAX_MIN/"
+
+def avg_max_min():
+
+    for base, dirs, files in os.walk(in_dir):
+        ##Read each file name
+        #variable file is the name of the file with the extension and we put it as the file name 
+        for file in files:
+            filename = file
+            
+            if file.endswith('.dat'):  
+                # with open(in_dir+file, 'r') as f:
+                #         lines = f.readlines()
+                #         num_lines = len([l for l in lines if l.strip(' \n') != ''])
+                # n = (len(open(in_dir+file).readlines()))
+                sun = filename[4:9]
+                print(sun)
+                
+                with open(in_dir+file,'r') as input_file1, open(out_dir + 'AVERAGE_MAX_MIN_' +sun+'.dat', 'w') as output_file:
+                    lmst_Array = []
+                    data_B_Array = []
+                    data_E_Array = []
+                    n = (len(open(in_dir+file).readlines()))
+                    output_file.write("[1]AVG_B [2]LMST_MAX_B [3]MAX_B [4]LMST_MIN_B [5]MIN_B [6]AVG_E [7]LMST_MAX_E [8]MAX_E [9]LMST_MIN_E [10]MIN_E \n")
+                    cont = 0
+                    q = 0
+
+                    for k in input_file1:
+                        
+                        col = k.split()
+                        if len(col)<4:
+                            continue
+                        else:
+                            if q == 0:
+                                q = 1
+                                continue
+                            else:
+                                
+                                lmst = col[0]
+                                lmst_Array.append(lmst)
+                                data_B = float(col[2])
+                                data_B_Array.append(data_B)
+                                data_E = float(col[4])
+                                data_E_Array.append(data_E)
+                                cont = cont + 1
+
+                                if cont == n-1:
+                                    
+                                    avg_b = sum(data_B_Array)/len(data_B_Array)
+                                    avg_e = sum(data_E_Array)/len(data_E_Array)
+
+                                    max_b = max(data_B_Array)
+                                    pos_max_b = data_B_Array.index(max_b)
+
+                                    max_e = max(data_E_Array)
+                                    pos_max_e = data_E_Array.index(max_e)
+                                    
+                                    min_b = min(data_B_Array)
+                                    pos_min_b = data_B_Array.index(min_b)
+                                    
+                                    min_e = min(data_E_Array)
+                                    pos_min_e = data_E_Array.index(min_e)
+
+                                    lmst_max_b = lmst_Array[pos_max_b]
+                                    lmst_max_e = lmst_Array[pos_max_e]
+                                    lmst_min_b = lmst_Array[pos_min_b]
+                                    lmst_min_e = lmst_Array[pos_min_e]
+
+                                    avg_b = round(float(avg_b), 4)
+                                    avg_e = round(float(avg_e), 4)
+
+                                    
+                                    output_file.write(str(avg_b) + ' ' + str(lmst_max_b)+ ' ' + str(max_b)+ ' ' + str(lmst_min_b)+ ' ' + str(min_b)+ ' ' + str(avg_e)+ ' ' + str(lmst_max_e)+ ' ' + str(max_e)+ ' ' + str(lmst_min_e)+ ' ' + str(min_e)+ '\n')
+                                
+
+avg_max_min()
+
